8d0b9ebfea6f69def46eaa745f13c65afed1b868
Nikita Baranov <nekit.banan1@mail.ru>
2021-11-17 12:40:22 +0300

    Окно для регистрации в марафоне

---
+++ Registration/RegistrationOnMarathon.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Controls;
+using Marafon.Database;
+
+namespace Marafon.Registration
+{
+    /// <summary>
+    /// Логика взаимодействия для RegistrationOnMarathon.xaml
+    /// </summary>
+    public partial class RegistrationOnMarathon : Page
+    {
+        public RegistrationOnMarathon()
+        {
+            InitializeComponent();
+
+            FundComboBox.DisplayMemberPath = "CharityName";
+            FundComboBox.ItemsSource = Context.GetFundsList();
+
+            FundInfoButton.Click += FundInfoButton_Click;
+        }
+
+        private void FundInfoButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var aboutPage = new AboutFundWindow();
+            aboutPage.Show();
+        }
+    }
+}
